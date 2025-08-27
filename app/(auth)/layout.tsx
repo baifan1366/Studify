86877245a55b59693ff2e9542aaf34e5110c7b25
XUN0024 <jiaxuanshe3966@gmail.com>
2025-08-27 17:36:23 +0800

    refactor: Redesign and refactor sign-in/sign-up page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,13 @@
-import Content from "@/components/content";
-
 export default function UnauthenticatedLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <Content>{children}</Content>;
+  return (
+    <div className="min-h-screen flex flex-col">
+      <main className="flex-grow flex items-center justify-center bg-[#F8F9FF]">
+        {children}
+      </main>
+    </div>
+  );
 }
